6574e81dd091a6c699d0ff9893ce31579deef9ed
Aamna_Bashir <aamnabashir47@gmail.com>
2020-09-05 13:09:48 -0700

    first commit

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,14 +2,28 @@ import React from 'react';
 import './App.css';
 import BusinessList from'../BusinessList/BusinessList';
 import SearchBar from '../SearchBar/SearchBar';
-
+const business={
+    imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+    name: 'Olivo Pizzeria',
+    address: '1010 Paddington Way',
+    city: 'Flavortown',
+    state: 'NY',
+    zipCode: '10101',
+    category: 'Italian',
+    rating: 4.5,
+    reviewCount: 90
+};
+const businesses=[business,business,business,business,business,business];
 class App extends React.Component{
+    searchYelp(term,location,sortBy){
+       console.log('You are searching for ${term},${location} and ${sorBy}');
+    }
     render(){
         return (
          <div className="App">
          <h1>ravenous</h1>
-         <SearchBar />
-         <BusinessList /> 
+         <SearchBar searchYelp={this.searchYelp} />
+         <BusinessList  businesses={businesses} /> 
          </div>
         );
         
